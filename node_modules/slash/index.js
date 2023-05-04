54d2cf50c12872c1e37532e68b426782b1bead0c
Bhavan Srinathan <bhavan@gmail.com>
2023-05-04 14:10:37 -0400

    removed node_modules

--- node_modules/slash/index.js
+++
@@ -1,9 +0,0 @@
-export default function slash(path) {
-	const isExtendedLengthPath = /^\\\\\?\\/.test(path);
-
-	if (isExtendedLengthPath) {
-		return path;
-	}
-
-	return path.replace(/\\/g, '/');
-}
